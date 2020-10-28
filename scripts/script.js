7b49319b0cf5854354b78cea73d73c0514379969
Winnie <70725454+chuio001@users.noreply.github.com>
2020-10-28 17:59:39 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 // JavaScript Document
+
+function showMenuItems() {
+    const toggleItem = document.getElementById('menu-btn');
+    const hamburgerItems = document.getElementById('hamburger-items');
+    toggleItem.addEventListener('click', function () {
+        this.classList.toggle('opened');
+        this.setAttribute('aria-expanded', this.classList.contains('opened'))
+        if (hamburgerItems.classList.contains('nav-show')) {
+            hamburgerItems.classList.remove('nav-show');
+        } else {
+            hamburgerItems.classList.add('nav-show');
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showMenuItems();
+});
